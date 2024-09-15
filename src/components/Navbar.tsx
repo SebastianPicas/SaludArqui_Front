c59df228e21a0a99659232dad3748bc4cd911b07
Sebastian Piñeros <sebas.picas08@gmail.com>
2024-09-15 13:21:19 -0500

    Agrega logo navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import reactLogo from '../assets/react.svg'
 
 const Navbar: React.FC = () => {
 return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-
+        <img src={reactLogo} className="logo react" alt="React logo" />
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
